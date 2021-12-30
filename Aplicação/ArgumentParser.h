75fc53e1aa85cdaa93ff00e48b4f8a4286763902
Isaac Araujo <isaac.tito3@gmail.com>
2021-12-30 16:21:08 -0300

    Obtendo Opções

--- Aplicação/ArgumentParser.h
+++ Aplicação/ArgumentParser.h
@@ -17,12 +17,24 @@ public:
 	// Registra uma Flag usada durante o parse.
 	void RegisterFlag(const std::string& flag);
 
+	// Registra uma Opção usada durante o parse.
+	void RegisterOption(const std::string& option);
+
 	// Obtem o valor de uma flag obtida durante o parse.
 	bool GetFlag(const std::string& flag);
+
+	// Obtem o valor de uma opção obtida durante o parse.
+	const std::string& GetOption(const std::string& option) const;
 	
+	// Obtem o valor de uma opção convertida para float.
+	float GetOptionAsFloat(const std::string& option) const;
+
 	// Faz o parse da lista de argumentos em argv.
 	void Parse(int argc, char* argv[]);
+
 	
 private:
+	// Declarando Dicionários para nossas Flags e Options.
 	std::map<std::string, bool> m_Flags;
+	std::map<std::string, std::string> m_Options;
 };
